53e667dbccaacd38232972150dc6cd3e47840b2f
Hakan Ozhan <ozhanhakan@gmail.com>
2022-06-20 10:06:55 +0300

    stage 2/5 completed

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.kt
@@ -1,11 +1,15 @@
 package tictactoe
 
 fun main() {
+    println("Enter cells: ")
+    val enterCells = readln()
+    println("-".repeat(enterCells.length))
     println(
         """
-X O X
-O X O
-X X O   
+| ${enterCells[0]} ${enterCells[1]} ${enterCells[2]} |
+| ${enterCells[3]} ${enterCells[4]} ${enterCells[5]} |
+| ${enterCells[6]} ${enterCells[7]} ${enterCells[8]} |
     """.trimIndent()
     )
+    println("-".repeat(enterCells.length))
 }
